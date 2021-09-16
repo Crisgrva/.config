361e29c61929ac078fc13cd1927ad15cf1727b9f
Crisgrva <crstngranada@gmail.com>
2021-09-16 17:26:58 -0500

    delete green

--- setup.sh
+++ setup.sh
@@ -5,44 +5,43 @@ then
 	echo "Sorry, you are not root."
 	exit 1
 fi
-GREEN='\033[0;32m'
-echo -e "${GREEN}Updating apt-get..."
+echo -e "Updating apt-get..."
 apt-get update
 apt-get upgrade
 
-echo -e "${GREEN}Installing cUrl..."
+echo -e "Installing cUrl..."
 #Install curl
 apt-get -y install curl
 
-echo -e "${GREEN}Installing NeoVim..."
+echo -e "Installing NeoVim..."
 #Install NeoVim
 apt-get install neovim
 
-echo -e "${GREEN}Installing Git..."
+echo -e "Installing Git..."
 #Install Git
 apt-get install git
 
-echo -e "${GREEN}Installing Build-Essencial..."
+echo -e "Installing Build-Essencial..."
 #Install Build Essencial -> Tools for C
 apt-get -y install build-essential
 
-echo -e "${GREEN}Installing Betty..."
+echo -e "Installing Betty..."
 #Install Betty
 git clone https://github.com/holbertonschool/Betty.git
 cd Betty/
 ./install.sh
 
-echo -e "${GREEN}Installing Python3, PIP & Pycodestyle..."
+echo -e "Installing Python3, PIP & Pycodestyle..."
 #Install Python3
 apt-get install python3
 apt-get install python3-pip
 pip install pycodestyle
 
-echo -e "${GREEN}Installing ZSH..."
+echo -e "Installing ZSH..."
 #Install ZSH
 apt-get install zsh
 
-echo -e "${GREEN}Installing OhMyZsh..."
+echo -e "Installing OhMyZsh..."
 #Install OhMyZsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
