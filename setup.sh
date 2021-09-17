4aff7aaada3ee6ff5625722a3235ede0a8dbc767
Crisgrva <crstngranada@gmail.com>
2021-09-16 20:30:56 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,38 @@ then
 	exit 1
 fi
 
+echo ""
+echo "   +-+-+-+-+ +-+-+-+-+-+-+ +-+-+-+-+ +-+-+ +-+-+-+ +-+-+-+-+-+-+-+-+-+ +-+-+
+   |T|h|i|s| |s|c|r|i|p|t| |w|i|l|l| |d|o| |t|h|e| |f|o|l|l|o|w|i|n|g| |o|n|
+   +-+-+-+-+ +-+-+-+-+-+-+ +-+-+-+-+ +-+-+ +-+-+-+ +-+-+-+-+-+-+-+-+-+ +-+-+
+                           +-+-+-+-+ +-+-+-+-+-+-+-+
+                           |y|o|u|r| |m|a|c|h|i|n|e|
+                           +-+-+-+-+ +-+-+-+-+-+-+-+"
+
+echo "✅1.  Unminimize Ubuntu"
+echo "✅2.  Update apt-get"
+echo "✅3.  Install cUrl"
+echo "✅4.  Install Git"
+echo "✅5.  Install Node 16.x"
+echo "✅6.  Install NeoVim with custom settings"
+echo "✅7.  Install Build Essential"
+echo "✅8.  Install Betty Style Guide"
+echo "✅9.  Install Python3"
+echo "✅10. Install Python3-PIP"
+echo "✅11. Install Pycodestyle"
+echo "✅12. Install ZSH"
+echo "✅13. Install OhMyZsh"
+echo ""
+echo "Made by @Crisgrva"
+echo ""
+read -p 'Do you wish to continue? [y/n]' YES
+
+if [ $YES == 'y' ]
+then
+  continue
+else
+  exit
+fi
 #Go to root
 cd ~
 
@@ -38,7 +70,7 @@ git clone https://github.com/Crisgrva/.config.git
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo -e "Installing Build-Essencial..."
+echo -e "Installing Build-Essential..."
 #Install Build Essencial -> Tools for C
 apt-get -y install build-essential
 
