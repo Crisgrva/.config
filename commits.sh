9b157833c010639adc9b3d06b8fa22224c3268ac
Cristian Granada <crisgrvc@Cristians-MacBook-Pro.local>
2021-12-30 10:29:25 -0500

    [ Commits.sh ] Update file with push option to current branch [ 12/30/2021/10:29 ]

---
+++ commits.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Clearing terminal
+clear
+
+# Reading arguments from prompt to commit
+read -p $'\e[33mFile or task modified: \e[0m ' head
+read -p $'\e[33mCommit body: \e[0m ' body
+
+# Getting date
+date=$(date +'%m/%d/%Y/%I:%M')
+
+# Setting Capitalize head and body
+head="$(tr '[:lower:]' '[:upper:]' <<< ${head:0:1})${head:1}"
+body="$(tr '[:lower:]' '[:upper:]' <<< ${body:0:1})${body:1}"
+
+
+# Making commit
+commit="[ $head ] $body [ $date ]"
+git commit -m "$commit"
+
+# Cheking if commit could be done
+if [ $? -eq 1 ]
+then
+    echo ""
+    echo $'\e[31mSomething went wrong...\e[0m'
+    echo ""
+    exit
+fi
+
+# Printing commit preview
+echo ""
+echo $'\e[32mNew commit = \e[0m'$'\e[32m[ '$head' ]'$'\e[0m' $body $'\e[32m[ '$date' ]' $'\e[0m'
+echo ""
+
+# Giving the option to push
+branch=$(git rev-parse --abbrev-ref HEAD)
+read -p $'\e[33mDo you want to push to '$branch'' $'\e[0m' choose
+
+if [ $choose = "y" ] || [ $choose = "Y" ]
+then
+    echo "Pushing to $branch...."
+    git push origin $(current_branch)
+fi
